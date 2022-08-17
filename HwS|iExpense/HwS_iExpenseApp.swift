b0d46dc745a60eefe538429d24898cdbb00d4aea
Daniel Wippermann <danielw83@live.de>
2022-08-17 15:48:10 +0200

    Initial Commit

---
+++ HwS|iExpense/HwS_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HwS_iExpenseApp.swift
+//  HwS|iExpense
+//
+//  Created by Daniel Wippermann on 17.08.22.
+//
+
+import SwiftUI
+
+@main
+struct HwS_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
